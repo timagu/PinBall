0b07b19aac22739a27743401243f3453064ca738
KoutaYamazawa <kouta0810277@gmail.com>
2018-03-10 17:14:57 +0900

    Lesson5課題
    よろしくお願い致します。

---
+++ Assets/Score.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    //スコアを表示するテキスト
+    private GameObject scoreText;
+    //得点
+    private int score = 0;
+
+    // Use this for initialization
+    void Start () {
+        //シーン中のscoreTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //トリガーモードで他のオブジェクトと接触した場合の処理
+    void OnCollisionEnter(Collision other)
+    {
+        //障害物に当たった場合
+            //大きな雲に当たった場合
+        if (other.gameObject.tag == "LargeCloudTag")
+        {
+            // スコアを加算
+            this.score += 50;
+            //ScoreText獲得した点数を表示
+            this.scoreText.GetComponent<Text>().text = "Score " + this.score + "pt";
+        }
+            //小さな星に当たった場合
+        else if (other.gameObject.tag == "SmallStarTag"){
+            this.score += 10;
+            this.scoreText.GetComponent<Text>().text = "Score " + this.score + "pt";
+            //大きな星に当たった場合
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.score += 20;
+            this.scoreText.GetComponent<Text>().text = "Score " + this.score + "pt";
+        }
+            //小さな雲に当たった場合
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            this.score += 30;
+            this.scoreText.GetComponent<Text>().text = "Score " + this.score + "pt";
+        }
+    }
+}
